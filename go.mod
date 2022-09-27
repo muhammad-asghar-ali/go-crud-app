f0bcfad2ae0f43e1cbb8c465d275d2f34fd184fe
unknown <alikhan137>
2022-09-27 00:24:51 -0700

    create and update function

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module go-crud-app
 
 go 1.18
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
-type Moive struct {
+type Movie struct {
 	ID       string    `json: "id"`
 	isbn     string    `json: "isbn"`
 	Title    string    `json: "title"`
@@ -22,7 +24,7 @@ type Director struct {
 }
 
 // movie slice
-var movies []Moive
+var movies []Movie
 
 // get Movies
 func getMovies(w http.ResponseWriter, r *http.Request) {
@@ -30,7 +32,7 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movies)
 }
 
-func getMoive(w http.ResponseWriter, r *http.Request) {
+func getMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 
@@ -56,21 +58,38 @@ func deleteMovie(w http.ResponseWriter, r *http.Request) {
 }
 
 func createMovie(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(10000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
 }
 
 func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = strconv.Itoa(rand.Intn(10000000))
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+		}
+	}
 
 }
 
 func main() {
 	r := mux.NewRouter()
 
-	movies = append(movies, Moive{ID: "1", isbn: "3453", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "doe"}})
-	movies = append(movies, Moive{ID: "2", isbn: "3463", Title: "Movie two", Director: &Director{Firstname: "John", Lastname: "smith"}})
+	movies = append(movies, Movie{ID: "1", isbn: "3453", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "doe"}})
+	movies = append(movies, Movie{ID: "2", isbn: "3463", Title: "Movie two", Director: &Director{Firstname: "John", Lastname: "smith"}})
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
-	r.HandleFunc("/movies/{id}", getMoive).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
